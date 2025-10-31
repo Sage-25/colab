69ee94b5ab0be63f671dc7b6e56eba357400e748
CHINEDUDSN <okekechinedudaniel@gmail.com>
2025-10-31 08:03:31 +0100

    update to the js folder

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 console.log('Javascript is next!')
+
+console.log("Chinedu Okeke")
+
+console.log('20 + 2')
+
+100
+10.5
+
+true
+false
+
+// Numbers
+// String
+// Boolean
+// Variables
+// Variable reassignement
+
+// var numberStore = 46;
+// console.log (numberStore)
+
+// let numberBox = 100;
+
+// const numberRoom = 23;
+
+// let nameOfStudent = "Brain Brain";
+// let ageOfStudent = 33;
+
+// let firstName = "Brian";
+// let lastName = "Okafor";
+// let age = 20;
+// let homeAddress = "32, Ajubade Street";
+
+let number1 = 30;
+let number2 = 20;
+let number3 = 10;
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
+
+let ageOfStudent = 40;
+let nameOfStudent = "Johnson Blue";
+let hasStudentPaid = false;
+let studentRegNo = "HG4958DF";
+
+console.log(ageOfStudent);
+console.log(nameOfStudent);
+console.log(hasStudentPaid);
+console.log(studentRegNo);
