90fe352e73c2fe13073f9892a594d94d7b2e8c1d
CHINEDUDSN <okekechinedudaniel@gmail.com>
2025-11-03 20:07:40 +0100

    update to the js class

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-console.log('Javascript is next!')
+// console.log('Javascript is next!')
 
-console.log("Chinedu Okeke")
+// console.log("Chinedu Okeke")
 
-console.log('20 + 2')
+// console.log('20 + 2')
 
-100
-10.5
+// 100
+// 10.5
 
-true
-false
+// true
+// false
 
 // Numbers
 // String
@@ -31,20 +31,44 @@ false
 // let age = 20;
 // let homeAddress = "32, Ajubade Street";
 
-let number1 = 30;
-let number2 = 20;
-let number3 = 10;
+// let number1 = 30;
+// let number2 = 20;
+// let number3 = 10;
 
-console.log(number1);
-console.log(number2);
-console.log(number3);
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
 
-let ageOfStudent = 40;
-let nameOfStudent = "Johnson Blue";
-let hasStudentPaid = false;
-let studentRegNo = "HG4958DF";
+// let ageOfStudent = 40;
+// let nameOfStudent = "Johnson Blue";
+// let hasStudentPaid = false;
+// let studentRegNo = "HG4958DF";
 
-console.log(ageOfStudent);
-console.log(nameOfStudent);
-console.log(hasStudentPaid);
-console.log(studentRegNo);
+// console.log(ageOfStudent);
+// console.log(nameOfStudent);
+// console.log(hasStudentPaid);
+// console.log(studentRegNo);
+
+let number = 10
+number ++
+console.log(number);
+
+let newNumber = 20
+newNumber --
+console.log(newNumber);
+
+let mainNumber = 30
+mainNumber +=2
+console.log(mainNumber);
+
+let anotherNumber = 25
+anotherNumber -= 5
+console.log(anotherNumber);
+
+let gameScore = 200
+gameScore *= 10
+console.log(gameScore);
+
+let gameNewScore = 5000
+gameNewScore /= 15
+console.log(gameNewScore);
